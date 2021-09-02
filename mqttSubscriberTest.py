ebb50f88b74c8da2274bdd78032554da135a8ab4
julian <julian.sosa@usal.edu.ar>
2021-09-02 20:01:52 -0300

    no message

--- mqttSubscriberTest.py
+++ mqttSubscriberTest.py
@@ -1,7 +1,7 @@
 from mqttPublisherTest import connect_mqtt
 from paho.mqtt import client as mqtt_client
 
-topic="/python/mqtt"
+topic="/casa/ambiente1"
 
 def subscribe(client: mqtt_client):
     def on_message(client, userdata, msg):
