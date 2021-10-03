6dfa49ef0661d819c0d0daf60dc01904b555e0ae
agoslomoro <agostina.lomoro@gmail.com>
2021-10-03 16:22:47 -0300

    Cleaning Code

--- mondodbTest.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-# pprint library is used to make the output look more pretty
-from pprint import pprint
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-client = MongoClient("mongodb+srv://admin:O3ByoOtAKMH01ZrM@sistemergentes.slhwd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",ssl=True,ssl_cert_reqs='CERT_NONE')
-db=client.admin
-# Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
